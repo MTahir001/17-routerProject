78c0000683dacda29a94dca906f81f9159b205e0
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-03-19 08:18:16 +0530

    route layout, nesting, error ele

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import RootLayout from "./pages/RootLayout.js";
+import ErrorPage from "./pages/Error.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hey... this is home page</h1>
+    </div>
+  );
+}
